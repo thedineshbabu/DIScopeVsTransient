24625a07b1833648be85c971442b68100555b58e
thedineshbabu <thedineshbabu@hotmail.com>
2020-06-11 15:36:23 -0500

    WIP

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ScopeVsTransient
+{
+    public class ScopedClass{}
+    public class TransientClass{}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new ServiceCollection();
+            collection.AddScoped<ScopedClass>();
+            collection.AddTransient<TransientClass>();
+
+            var builder = collection.BuildServiceProvider();
+
+            Console.Clear();
+
+            Parallel.For(1, 10, i=>{
+                Console.WriteLine("ScopedId: " + builder.GetService<ScopedClass>().GetHashCode().ToString());
+                Console.WriteLine("TransientId: " + builder.GetService<TransientClass>().GetHashCode().ToString());
+            });
+        }
+    }
+}
